205ec2c7b5786a5e33c0f52a73c1d32d4efa3d2e
Ezequiel Gottelli <ezepincha@gmail.com>
2020-12-29 18:35:38 -0300

    New example of Observer

---
+++ Observer/EventObserver/Test/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../Entity/Observer.php';
+
+$repository = new UserRepository();
+$repository->attach(new Logger(__DIR__ . "/log.txt"), "*");
+$repository->attach(new OnboardingNotification("1@example.com"), "users:created");
+
+$repository->initialize(__DIR__ . "/users.csv");
+
+$user = $repository->createUser([
+    "name" => "John Smith",
+    "email" => "john99@example.com"
+]);
+
+$repository->deleteUser($user);
+?>
