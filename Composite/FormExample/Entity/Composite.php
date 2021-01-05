e9d5404732019f262761a372d4865aee422c04ce
Ezequiel Gottelli <ezepincha@gmail.com>
2021-01-05 17:57:41 -0300

    More Composite example

---
+++ Composite/FormExample/Entity/Composite.php
@@ -0,0 +1,181 @@
+<?php
+
+/**
+ * The base Component class declares an interface for all concrete components,
+ * both simple and complex.
+ *
+ * In our example, we'll be focusing on the rendering behavior of DOM elements.
+ */
+abstract class FormElement
+{
+    /**
+     * We can anticipate that all DOM elements require these 3 fields.
+     */
+    protected $name;
+    protected $title;
+    protected $data;
+
+    public function __construct(string $name, string $title)
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setData($data): void
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Each concrete DOM element must provide its rendering implementation, but
+     * we can safely assume that all of them are returning strings.
+     */
+    abstract public function render(): string;
+}
+
+/**
+ * This is a Leaf component. Like all the Leaves, it can't have any children.
+ */
+class Input extends FormElement
+{
+    private $type;
+
+    public function __construct(string $name, string $title, string $type)
+    {
+        parent::__construct($name, $title);
+        $this->type = $type;
+    }
+
+    /**
+     * Since Leaf components don't have any children that may handle the bulk of
+     * the work for them, usually it is the Leaves who do the most of the heavy-
+     * lifting within the Composite pattern.
+     */
+    public function render(): string
+    {
+        return "<label for=\"{$this->name}\">{$this->title}</label>\n" .
+            "<input name=\"{$this->name}\" type=\"{$this->type}\" value=\"{$this->data}\">\n";
+    }
+}
+
+/**
+ * The base Composite class implements the infrastructure for managing child
+ * objects, reused by all Concrete Composites.
+ */
+abstract class FieldComposite extends FormElement
+{
+    /**
+     * @var FormElement[]
+     */
+    protected $fields = [];
+
+    /**
+     * The methods for adding/removing sub-objects.
+     */
+    public function add(FormElement $field): void
+    {
+        $name = $field->getName();
+        $this->fields[$name] = $field;
+    }
+
+    public function remove(FormElement $component): void
+    {
+        $this->fields = array_filter($this->fields, function ($child) use ($component) {
+            return $child != $component;
+        });
+    }
+
+    /**
+     * Whereas a Leaf's method just does the job, the Composite's method almost
+     * always has to take its sub-objects into account.
+     *
+     * In this case, the composite can accept structured data.
+     *
+     * @param array $data
+     */
+    public function setData($data): void
+    {
+        foreach ($this->fields as $name => $field) {
+            if (isset($data[$name])) {
+                $field->setData($data[$name]);
+            }
+        }
+    }
+
+    /**
+     * The same logic applies to the getter. It returns the structured data of
+     * the composite itself (if any) and all the children data.
+     */
+    public function getData(): array
+    {
+        $data = [];
+        
+        foreach ($this->fields as $name => $field) {
+            $data[$name] = $field->getData();
+        }
+        
+        return $data;
+    }
+
+    /**
+     * The base implementation of the Composite's rendering simply combines
+     * results of all children. Concrete Composites will be able to reuse this
+     * implementation in their real rendering implementations.
+     */
+    public function render(): string
+    {
+        $output = "";
+        
+        foreach ($this->fields as $name => $field) {
+            $output .= $field->render();
+        }
+        
+        return $output;
+    }
+}
+
+/**
+ * The fieldset element is a Concrete Composite.
+ */
+class Fieldset extends FieldComposite
+{
+    public function render(): string
+    {
+        // Note how the combined rendering result of children is incorporated
+        // into the fieldset tag.
+        $output = parent::render();
+        
+        return "<fieldset><legend>{$this->title}</legend>\n$output</fieldset>\n";
+    }
+}
+
+/**
+ * And so is the form element.
+ */
+class Form extends FieldComposite
+{
+    protected $url;
+
+    public function __construct(string $name, string $title, string $url)
+    {
+        parent::__construct($name, $title);
+        $this->url = $url;
+    }
+
+    public function render(): string
+    {
+        $output = parent::render();
+        return "<form action=\"{$this->url}\">\n<h3>{$this->title}</h3>\n$output</form>\n";
+    }
+}
+?>
